9143d30e935bfc50586458d877263f22279d6494
kim beom seok <qjatjr1285@naver.com>
2023-06-20 16:38:37 +0900

    20230620 django

---
+++ Django/myapp/blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+# from blog.views import Index
+
+urlpatterns = [
+    # path(패턴, 매핑) /blog/
+    # path("", views.index), # FBV
+    path("", views.Index.as_view()),
+    # 글 조회
+    # 글 작성
+    # 글 수정
+    # 글 삭제
+    # 코멘트 작성
+    # 코멘트 삭제
+]
