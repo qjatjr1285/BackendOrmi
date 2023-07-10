b8cc5a4c344ed0508413978c6cc5f85aedd9ef1d
kim beom seok <qjatjr1285@naver.com>
2023-07-10 17:16:18 +0900

    20230710 django 수업

--- Django/restapp/user/models.py
+++ Django/restapp/user/models.py
@@ -57,3 +57,7 @@ class User(AbstractUser):
     
     # def __str__(self):
     #     return self.name
+
+    class Profile(models.Model):
+        user = models.ForeignKey('User', on_delete=models.CASCADE)
+        image = models.ImageField(upload_to='user/media')
